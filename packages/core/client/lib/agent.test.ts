7aae8ef728591f3a5d898206fc92348871d9f0e5
Mary <git@mary.my.id>
2024-08-27 14:57:18 +0700

    chore(client): add more tests

--- packages/core/client/lib/agent.test.ts
+++ packages/core/client/lib/agent.test.ts
@@ -1,18 +1,25 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
 
 import { TestNetwork } from '@atcute/internal-dev-env';
 
-import { XRPC } from './index.js';
-import { AtpAuth } from './middlewares/auth.js';
+import { XRPC, XRPCError } from './index.js';
+import { AtpAuth, type AtpSessionData } from './middlewares/auth.js';
 
 let network: TestNetwork;
 
-beforeEach(async () => {
+beforeAll(async () => {
 	network = await TestNetwork.create({});
+
+	const rpc = new XRPC({ service: network.pds.url });
+	await createAccount(rpc, 'user1.test');
 });
 
-afterEach(async () => {
+afterAll(async () => {
 	await network.close();
+});
+
+afterEach(() => {
+	vi.restoreAllMocks();
 });
 
 it('can connect to a PDS', async () => {
@@ -33,15 +40,6 @@ it('can connect to a PDS', async () => {
 });
 
 describe('AtpAuth', () => {
-	beforeEach(async () => {
-		const rpc = new XRPC({ service: network.pds.url });
-		await createAccount(rpc, 'user1.test');
-	});
-
-	afterEach(() => {
-		vi.restoreAllMocks();
-	});
-
 	it('can login', async () => {
 		const onSessionUpdate = vi.fn();
 
@@ -148,6 +146,98 @@ describe('AtpAuth', () => {
 		const refreshedJwt = auth.session!.accessJwt;
 
 		expect(refreshedJwt).not.toBe(originalJwt);
+	});
+
+	it('does not mutate session if refresh fails', async () => {
+		const originalFetch = globalThis.fetch;
+
+		const fetch = vi.spyOn(globalThis, 'fetch');
+		const onRefresh = vi.fn();
+
+		const rpc = new XRPC({ service: network.pds.url });
+		const auth = new AtpAuth(rpc, { onRefresh: onRefresh });
+
+		await auth.login({ identifier: 'user1.test', password: 'password' });
+
+		const originalJwt = auth.session!.accessJwt;
+
+		// Refreshing now would return the same token due to matching timestamp,
+		// wait for 1 second.
+		await sleep(1_000);
+
+		await fetch.withImplementation(
+			(input, init) => {
+				const request = new Request(input, init);
+
+				if (request.headers.get('authorization') === `Bearer ${originalJwt}`) {
+					return Promise.resolve(
+						new Response(JSON.stringify({ error: 'ExpiredToken' }), {
+							status: 400,
+							headers: { 'Content-Type': 'application/json' },
+						}),
+					);
+				}
+
+				if (request.url.includes('/xrpc/com.atproto.server.refreshSession')) {
+					return Promise.resolve(new Response(undefined, { status: 500 }));
+				}
+
+				return originalFetch(request);
+			},
+			async () => {
+				try {
+					await rpc.get('com.atproto.server.getSession', {});
+					expect.fail(`getSession call should not succeed`);
+				} catch (err) {
+					if (!(err instanceof XRPCError)) {
+						expect.fail(`No errors other than XRPC error should be thrown`);
+					}
+
+					expect(err.kind).toBe('ExpiredToken');
+				}
+			},
+		);
+
+		expect(auth.session).not.toBe(undefined);
+		expect(auth.session!.accessJwt).toBe(originalJwt);
+
+		expect(onRefresh).not.toHaveBeenCalled();
+	});
+
+	it('can resume sessions', async () => {
+		let session: AtpSessionData;
+
+		{
+			const rpc = new XRPC({ service: network.pds.url });
+			const auth = new AtpAuth(rpc, {});
+
+			await auth.login({ identifier: 'user1.test', password: 'password' });
+
+			expect(auth.session).not.toBe(undefined);
+			session = auth.session!;
+		}
+
+		const fetch = vi.spyOn(globalThis, 'fetch');
+		expect(fetch).not.toHaveBeenCalled();
+
+		{
+			const rpc = new XRPC({ service: network.pds.url });
+			const auth = new AtpAuth(rpc, {});
+
+			await auth.resume(session);
+
+			expect(auth.session).not.toBe(undefined);
+		}
+
+		expect(fetch).toHaveBeenCalledOnce();
+		expect(fetch.mock.lastCall).not.toBe(undefined);
+
+		{
+			const lastCall = fetch.mock.lastCall!;
+			const request = new Request(lastCall[0], lastCall[1]);
+
+			expect(request.url).includes('/xrpc/com.atproto.server.getSession');
+		}
 	});
 });
 
