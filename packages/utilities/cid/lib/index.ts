d07a423a0eabe5062dde0b5c45e981681deae5f7
Mary <git@mary.my.id>
2024-10-05 09:53:06 +0700

    feat(cid): expose encode and digest methods

--- packages/utilities/cid/lib/index.ts
+++ packages/utilities/cid/lib/index.ts
@@ -141,7 +141,7 @@ export const decode = (bytes: Uint8Array): CID => {
  */
 export const create = async (code: number, input: Uint8Array): Promise<CID> => {
 	const digest = createDigest(0x12, new Uint8Array(await crypto.subtle.digest('sha-256', input)));
-	const bytes = encodeCID(1, code, digest.bytes);
+	const bytes = encode(1, code, digest.bytes);
 
 	return {
 		version: 1,
@@ -158,7 +158,7 @@ export const format = (cid: CID): string => {
 	return 'b' + base32.encode(cid.bytes);
 };
 
-const createDigest = (code: number, digest: Uint8Array): Digest => {
+export const createDigest = (code: number, digest: Uint8Array): Digest => {
 	const size = digest.byteLength;
 	const sizeOffset = varint.encodingLength(code);
 	const digestOffset = sizeOffset + varint.encodingLength(size);
@@ -176,7 +176,7 @@ const createDigest = (code: number, digest: Uint8Array): Digest => {
 	};
 };
 
-const encodeCID = (version: number, code: number, multihash: Uint8Array): Uint8Array => {
+export const encode = (version: number, code: number, multihash: Uint8Array): Uint8Array => {
 	const codeOffset = varint.encodingLength(version);
 	const hashOffset = codeOffset + varint.encodingLength(code);
 
