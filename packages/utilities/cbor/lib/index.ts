01a5f7488eb54bb6efcde02107162c1a3cd3f48d
Mary <git@mary.my.id>
2024-10-05 09:06:01 +0700

    feat(cbor): export byte and cid-link interfaces

--- packages/utilities/cbor/lib/index.ts
+++ packages/utilities/cbor/lib/index.ts
@@ -1,5 +1,5 @@
-export { fromBytes, toBytes } from './bytes.js';
-export { fromCIDLink, toCIDLink } from './cid-link.js';
+export { BytesWrapper, fromBytes, toBytes, type Bytes } from './bytes.js';
+export { CIDLinkWrapper, fromCIDLink, toCIDLink, type CIDLink } from './cid-link.js';
 
 export { decode, decodeFirst } from './decode.js';
 export { encode } from './encode.js';
