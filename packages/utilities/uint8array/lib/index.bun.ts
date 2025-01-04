d800500c35230e6686c7b85c121304b04993fb88
Mary <git@mary.my.id>
2025-01-04 13:02:21 +0700

    feat(uint8array): bun-specific optimization

---
+++ packages/utilities/uint8array/lib/index.bun.ts
@@ -0,0 +1,46 @@
+import { allocUnsafe as _allocUnsafe, concatArrayBuffers as _concat } from 'bun';
+
+import { Buffer as NodeBuffer } from 'node:buffer';
+import { timingSafeEqual as _timingSafeEqual } from 'node:crypto';
+
+const _compare = /*#__PURE__*/ NodeBuffer.prototype.compare;
+const _equals = /*#__PURE__*/ NodeBuffer.prototype.equals;
+const _utf8Slice = /*#__PURE__*/ NodeBuffer.prototype.utf8Slice;
+const _utf8Write = /*#__PURE__*/ NodeBuffer.prototype.utf8Write;
+
+export const alloc = (size: number): Uint8Array => {
+	return new Uint8Array(size);
+};
+
+export const allocUnsafe = (size: number): Uint8Array => {
+	return _allocUnsafe(size);
+};
+
+export const compare = (a: Uint8Array, b: Uint8Array): number => {
+	return _compare.call(a, b);
+};
+
+export const equals = (a: Uint8Array, b: Uint8Array): boolean => {
+	return _equals.call(a, b);
+};
+
+export const timingSafeEquals = (a: Uint8Array, b: Uint8Array): boolean => {
+	return _timingSafeEqual(a, b);
+};
+
+export const concat = (arrays: Uint8Array[], size?: number): Uint8Array => {
+	// Bun's typings is slightly wrong, *you can* pass `size: undefined` with `asUint8Array: true`
+	return _concat(arrays, size as number, true);
+};
+
+export const encodeUtf8Into = (to: Uint8Array, str: string, offset?: number, length?: number): number => {
+	return _utf8Write.call(to, str, offset, length);
+};
+
+export const decodeUtf8From = (
+	from: Uint8Array,
+	offset: number = 0,
+	length: number = from.length,
+): string => {
+	return _utf8Slice.call(from, offset, offset + length);
+};
