5b0fecc22ac4ee577ee974180a346ad00ad1739d
Mary <git@mary.my.id>
2025-01-04 13:02:45 +0700

    refactor(uint8array): mess around with the node optimization

--- packages/utilities/uint8array/lib/index.node.ts
+++ packages/utilities/uint8array/lib/index.node.ts
@@ -1,36 +1,42 @@
 import { Buffer as NodeBuffer } from 'node:buffer';
-import * as crypto from 'node:crypto';
+import { timingSafeEqual as _timingSafeEqual } from 'node:crypto';
+
+const _alloc = /*#__PURE__*/ NodeBuffer.alloc;
+const _allocUnsafe = /*#__PURE__*/ NodeBuffer.allocUnsafe;
+const _concat = /*#__PURE__*/ NodeBuffer.concat;
+
+const _compare = /*#__PURE__*/ NodeBuffer.prototype.compare;
+const _equals = /*#__PURE__*/ NodeBuffer.prototype.equals;
+const _utf8Slice = /*#__PURE__*/ NodeBuffer.prototype.utf8Slice;
+const _utf8Write = /*#__PURE__*/ NodeBuffer.prototype.utf8Write;
 
 const toUint8Array = (buffer: NodeBuffer) => {
 	return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
 };
 
 export const alloc = (size: number): Uint8Array => {
-	return toUint8Array(NodeBuffer.alloc(size));
+	return toUint8Array(_alloc(size));
 };
 
 export const allocUnsafe = (size: number): Uint8Array => {
-	return toUint8Array(NodeBuffer.allocUnsafe(size));
+	return toUint8Array(_allocUnsafe(size));
 };
 
 export const compare = (a: Uint8Array, b: Uint8Array): number => {
-	return NodeBuffer.compare(a, b);
+	return _compare.call(a, b);
 };
 
 export const equals = (a: Uint8Array, b: Uint8Array): boolean => {
-	return NodeBuffer.compare(a, b) === 0;
+	return _equals.call(a, b);
 };
 
 export const timingSafeEquals = (a: Uint8Array, b: Uint8Array): boolean => {
-	return crypto.timingSafeEqual(a, b);
+	return _timingSafeEqual(a, b);
 };
 
 export const concat = (arrays: Uint8Array[], size?: number): Uint8Array => {
-	return toUint8Array(Buffer.concat(arrays, size));
+	return toUint8Array(_concat(arrays, size));
 };
-
-const _utf8Write = /*#__PURE__*/ NodeBuffer.prototype.utf8Write;
-const _utf8Slice = /*#__PURE__*/ NodeBuffer.prototype.utf8Slice;
 
 export const encodeUtf8Into = (to: Uint8Array, str: string, offset?: number, length?: number): number => {
 	return _utf8Write.call(to, str, offset, length);
