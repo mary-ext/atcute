c90b8add2419b1463ca025d8fbece1790808136f
Mary <git@mary.my.id>
2025-01-08 10:48:40 +0700

    refactor(multibase): don't export internal functions

--- packages/utilities/multibase/lib/index.ts
+++ packages/utilities/multibase/lib/index.ts
@@ -1,4 +1,14 @@
-export * from '#bases/base16';
-export * from '#bases/base64';
-export * from './bases/base32.js';
-export * from './bases/base58.js';
+export { fromBase16, toBase16 } from '#bases/base16';
+export {
+	fromBase64,
+	fromBase64Pad,
+	fromBase64Url,
+	fromBase64UrlPad,
+	toBase64,
+	toBase64Pad,
+	toBase64Url,
+	toBase64UrlPad,
+} from '#bases/base64';
+
+export { fromBase32, toBase32 } from './bases/base32.js';
+export { fromBase58Btc, toBase58Btc } from './bases/base58.js';
