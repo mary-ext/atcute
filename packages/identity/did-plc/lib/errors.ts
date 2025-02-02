3fa440dd79ff290e91e26afd922ceee63e406074
Mary <git@mary.my.id>
2025-02-02 22:35:15 +0700

    fix(did-plc): oops

--- packages/identity/did-plc/lib/errors.ts
+++ packages/identity/did-plc/lib/errors.ts
@@ -3,31 +3,31 @@ import * as t from './types.js';
 export class PlcError extends Error {}
 
 export class ImproperOperationError extends PlcError {
-	constructor(operation: t.IndexedOperation, reason: string) {
+	constructor(operation: t.IndexedEntry, reason: string) {
 		super(`improper operation; cid=${operation.cid}; reason=${reason}`);
 	}
 }
 
 export class InvalidSignatureError extends PlcError {
-	constructor(operation: t.IndexedOperation) {
+	constructor(operation: t.IndexedEntry) {
 		super(`invalid signature; cid=${operation.cid}`);
 	}
 }
 
 export class InvalidHashError extends PlcError {
-	constructor(operation: t.IndexedOperation, expected: string) {
+	constructor(operation: t.IndexedEntry, expected: string) {
 		super(`invalid hash; expected=${expected}; got=${operation.cid}`);
 	}
 }
 
 export class GenesisHashError extends PlcError {
-	constructor(operation: t.IndexedOperation, did: t.DidPlcString) {
+	constructor(operation: t.IndexedEntry, did: t.DidPlcString) {
 		super(`mismatching genesis hash; did=${did}; cid=${operation.cid}`);
 	}
 }
 
 export class LateRecoveryError extends PlcError {
-	constructor(operation: t.IndexedOperation, lapsed: number) {
+	constructor(operation: t.IndexedEntry, lapsed: number) {
 		super(`recovery operation occured outside of recovery window; cid=${operation.cid}; lapsed=${lapsed}`);
 	}
 }
