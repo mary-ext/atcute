0cdaf8d795d5727a86ea00ac0deb9c3ac6fa125c
Mary <git@mary.my.id>
2025-02-04 22:28:37 +0700

    refactor(identity): alter handle regex a little

--- packages/identity/identity/lib/handle.ts
+++ packages/identity/identity/lib/handle.ts
@@ -1,7 +1,7 @@
 import type { Handle } from './types.js';
 
 export const HANDLE_RE =
-	/^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/;
+	/^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+([a-zA-Z][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])$/;
 
 export const isHandle = (str: string): str is Handle => {
 	return str.length >= 3 && str.length <= 253 && HANDLE_RE.test(str);
