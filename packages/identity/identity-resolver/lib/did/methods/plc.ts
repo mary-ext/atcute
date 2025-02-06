7a833b9aadb81cafb846a0ee14b2bc2194c315e5
Mary <git@mary.my.id>
2025-02-06 08:08:56 +0700

    refactor(identity-resolver): make apiUrl exposed but readonly

--- packages/identity/identity-resolver/lib/did/methods/plc.ts
+++ packages/identity/identity-resolver/lib/did/methods/plc.ts
@@ -9,16 +9,16 @@ export interface PlcDidResolverOptions {
 }
 
 export class PlcDidResolver implements DidResolver<'plc'> {
-	#apiUrl: string;
+	readonly apiUrl: string;
 	#fetch: typeof fetch;
 
 	constructor({ apiUrl = 'https://plc.directory', fetch: fetchThis = fetch }: PlcDidResolverOptions = {}) {
-		this.#apiUrl = apiUrl;
+		this.apiUrl = apiUrl;
 		this.#fetch = fetchThis;
 	}
 
 	async resolve(did: Did<'plc'>, options?: ResolveDidOptions): Promise<DidDocument> {
-		const url = new URL(`/${encodeURIComponent(did)}`, this.#apiUrl);
+		const url = new URL(`/${encodeURIComponent(did)}`, this.apiUrl);
 
 		const response = await (0, this.#fetch)(url, {
 			signal: options?.signal,
