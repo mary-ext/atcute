90514f477efa0e6ea206859735af931577fab70f
Mary <git@mary.my.id>
2025-05-04 08:53:02 +0700

    feat(lexicons): export more useful stuff through main entry

--- packages/core/lexicons/lib/index.ts
+++ packages/core/lexicons/lib/index.ts
@@ -1,5 +1,14 @@
 export type { ActorIdentifier } from './syntax/at-identifier.js';
-export type { CanonicalResourceUri, ResourceUri } from './syntax/at-uri.js';
+export {
+	isCanonicalResourceUri,
+	isResourceUri,
+	parseCanonicalResourceUri,
+	parseResourceUri,
+	type CanonicalResourceUri,
+	type ParsedCanonicalResourceUri,
+	type ParsedResourceUri,
+	type ResourceUri,
+} from './syntax/at-uri.js';
 export type { Cid } from './syntax/cid.js';
 export type { Datetime } from './syntax/datetime.js';
 export type { Did } from './syntax/did.js';
@@ -15,3 +24,5 @@ export type { Bytes } from './interfaces/bytes.js';
 export type { CidLink } from './interfaces/cid-link.js';
 
 export type { $type } from './types/brand.js';
+
+export { is, parse, safeParse, ValidationError } from './validations/parse.js';
