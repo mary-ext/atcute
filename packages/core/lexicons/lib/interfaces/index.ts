8873b3bea587040c9466ea4aec0537e5768e25f6
Mary <git@mary.my.id>
2025-05-04 08:38:21 +0700

    feat(lexicons): export all interfaces

---
+++ packages/core/lexicons/lib/interfaces/index.ts
@@ -0,0 +1,3 @@
+export { isBlob, isLegacyBlob, type Blob, type LegacyBlob } from './blob.js';
+export { isBytes, type Bytes } from './bytes.js';
+export { isCidLink, type CidLink } from './cid-link.js';
