1da640f6abf381c18a743c5e8e338c077a115f85
Mary <git@mary.my.id>
2025-05-13 16:02:31 +0700

    chore(lexicons): add kitchen sink validation test

---
+++ packages/core/lexicons/lib/validations/index.test.ts
@@ -0,0 +1,70 @@
+import { it } from 'vitest';
+
+import * as v from './index.js';
+
+it('performs validation', () => {
+	const subobjectSchema = v.object({
+		$type: v.optional(v.literal('com.example.kitchenSink#subobject')),
+		boolean: v.boolean(),
+	});
+
+	const objectSchema = v.object({
+		$type: v.optional(v.literal('com.example.kitchenSink#object')),
+		get object() {
+			return subobjectSchema;
+		},
+		array: v.array(v.string()),
+		boolean: v.boolean(),
+		integer: v.integer(),
+		string: v.string(),
+	});
+
+	const recordSchema = v.record(
+		v.tidString(),
+		v.object({
+			$type: v.literal('com.example.kitchenSink'),
+			get object() {
+				return objectSchema;
+			},
+			array: v.array(v.string()),
+			boolean: v.boolean(),
+			integer: v.integer(),
+			string: v.string(),
+
+			atUri: v.resourceUriString(),
+			datetime: v.datetimeString(),
+			did: v.didString(),
+			cid: v.cidString(),
+
+			bytes: v.bytes(),
+			cidLink: v.cidLink(),
+		}),
+	);
+
+	const res: v.InferInput<typeof recordSchema> = {
+		$type: 'com.example.kitchenSink',
+		object: {
+			object: { boolean: true },
+			array: ['one', 'two'],
+			boolean: true,
+			integer: 123,
+			string: 'string',
+		},
+		array: ['one', 'two'],
+		boolean: true,
+		integer: 123,
+		string: 'string',
+		datetime: new Date().toISOString(),
+		atUri: 'at://did:web:example.com/com.example.test/self',
+		did: 'did:web:example.com',
+		cid: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+		bytes: {
+			$bytes: 'AAECAw',
+		},
+		cidLink: {
+			$link: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+		},
+	};
+
+	v.parse(recordSchema, res);
+});
