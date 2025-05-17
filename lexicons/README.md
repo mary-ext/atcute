a53b88aa451e8d1eadd47a8edb8a6619d8bdc7e9
Mary <git@mary.my.id>
2025-05-17 08:06:12 +0700

    chore: pull latest Bluesky lexicons

--- lexicons/README.md
+++ lexicons/README.md
@@ -1,1 +1,1 @@
-https://github.com/bluesky-social/atproto/tree/3a65b68f7dc63c8bfbea0ae615f8ae984272f2e4/lexicons
+https://github.com/bluesky-social/atproto/tree/86b3153884099ceeb0cfdb9d2bfdd447c39fb35a/lexicons
--- packages/definitions/bluesky/lib/lexicons/types/app/bsky/unspecced/getConfig.ts
+++ packages/definitions/bluesky/lib/lexicons/types/app/bsky/unspecced/getConfig.ts
@@ -2,21 +2,34 @@ import type {} from '@atcute/lexicons';
 import * as v from '@atcute/lexicons/validations';
 import type {} from '@atcute/lexicons/ambient';
 
+const _liveNowConfigSchema = /*#__PURE__*/ v.object({
+	$type: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.literal('app.bsky.unspecced.getConfig#liveNowConfig')),
+	did: /*#__PURE__*/ v.didString(),
+	domains: /*#__PURE__*/ v.array(/*#__PURE__*/ v.string()),
+});
 const _mainSchema = /*#__PURE__*/ v.query('app.bsky.unspecced.getConfig', {
 	params: null,
 	output: {
 		type: 'lex',
 		schema: /*#__PURE__*/ v.object({
 			checkEmailConfirmed: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.boolean()),
+			get liveNow() {
+				return /*#__PURE__*/ v.optional(/*#__PURE__*/ v.array(liveNowConfigSchema));
+			},
 		}),
 	},
 });
 
+type liveNowConfig$schematype = typeof _liveNowConfigSchema;
 type main$schematype = typeof _mainSchema;
 
+export interface liveNowConfigSchema extends liveNowConfig$schematype {}
 export interface mainSchema extends main$schematype {}
 
+export const liveNowConfigSchema = _liveNowConfigSchema as liveNowConfigSchema;
 export const mainSchema = _mainSchema as mainSchema;
+
+export interface LiveNowConfig extends v.InferInput<typeof liveNowConfigSchema> {}
 
 declare module '@atcute/lexicons/ambient' {
 	interface XRPCQueries {
