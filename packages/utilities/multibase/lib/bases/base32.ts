0cc2e8d806374d10f6ac22ec729035877573c3fc
Mary <git@mary.my.id>
2025-06-04 18:11:29 +0700

    refactor(multibase): explicit types

--- packages/utilities/multibase/lib/bases/base32.ts
+++ packages/utilities/multibase/lib/bases/base32.ts
@@ -2,6 +2,14 @@ import { createRfc4648Decode, createRfc4648Encode } from '../utils.js';
 
 const BASE32_CHARSET = 'abcdefghijklmnopqrstuvwxyz234567';
 
-export const fromBase32 = /*#__PURE__*/ createRfc4648Decode(BASE32_CHARSET, 5, false);
+export const fromBase32: (source: string) => Uint8Array = /*#__PURE__*/ createRfc4648Decode(
+	BASE32_CHARSET,
+	5,
+	false,
+);
 
-export const toBase32 = /*#__PURE__*/ createRfc4648Encode(BASE32_CHARSET, 5, false);
+export const toBase32: (source: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
+	BASE32_CHARSET,
+	5,
+	false,
+);
--- packages/utilities/multibase/lib/bases/base58.ts
+++ packages/utilities/multibase/lib/bases/base58.ts
@@ -2,6 +2,8 @@ import { createBtcBaseDecode, createBtcBaseEncode } from '../utils.js';
 
 const BASE58BTC_CHARSET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
-export const fromBase58Btc = /*#__PURE__*/ createBtcBaseDecode(BASE58BTC_CHARSET);
+export const fromBase58Btc: (source: string) => Uint8Array =
+	/*#__PURE__*/ createBtcBaseDecode(BASE58BTC_CHARSET);
 
-export const toBase58Btc = /*#__PURE__*/ createBtcBaseEncode(BASE58BTC_CHARSET);
+export const toBase58Btc: (source: Uint8Array) => string =
+	/*#__PURE__*/ createBtcBaseEncode(BASE58BTC_CHARSET);
