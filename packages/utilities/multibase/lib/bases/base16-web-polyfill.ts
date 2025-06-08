2979cd8d89ae43e9e39efb38ed04c0e335567dfc
Mary <git@mary.my.id>
2025-06-08 08:12:14 +0700

    refactor(multibase): add explicit type declaration to the polyfill

--- packages/utilities/multibase/lib/bases/base16-web-polyfill.ts
+++ packages/utilities/multibase/lib/bases/base16-web-polyfill.ts
@@ -2,5 +2,13 @@ import { createRfc4648Decode, createRfc4648Encode } from '../utils.js';
 
 const BASE16_CHARSET = '0123456789abcdef';
 
-export const fromBase16 = /*#__PURE__*/ createRfc4648Decode(BASE16_CHARSET, 4, false);
-export const toBase16 = /*#__PURE__*/ createRfc4648Encode(BASE16_CHARSET, 4, false);
+export const fromBase16: (str: string) => Uint8Array = /*#__PURE__*/ createRfc4648Decode(
+	BASE16_CHARSET,
+	4,
+	false,
+);
+export const toBase16: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
+	BASE16_CHARSET,
+	4,
+	false,
+);
