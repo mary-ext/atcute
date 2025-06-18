cfbbc3e99226e0a7f7ceabe7c2800b33a1d08d67
Mary <git@mary.my.id>
2025-06-19 06:35:22 +0700

    fix(lexicons): handle syntax conformance

---
+++ .changeset/common-tips-behave.md
@@ -0,0 +1,5 @@
+---
+'@atcute/lexicons': patch
+---
+
+fix handle syntax conformance
--- packages/lexicons/lexicons/lib/syntax/handle.ts
+++ packages/lexicons/lexicons/lib/syntax/handle.ts
@@ -5,7 +5,7 @@
 export type Handle = `${string}.${string}`;
 
 const HANDLE_RE =
-	/^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+([a-zA-Z][a-zA-Z0-9-]{0,61}[a-zA-Z])$/;
+	/^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/;
 
 // #__NO_SIDE_EFFECTS__
 export const isHandle = (input: unknown): input is Handle => {
