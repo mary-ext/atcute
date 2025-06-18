8a3747390a9e58e2249249496c6a1c34123ba3e8
Mary <git@mary.my.id>
2025-06-18 07:18:05 +0700

    chore(lexicons): test with and without eval

--- packages/lexicons/lexicons/lib/validations/index.test.ts
+++ packages/lexicons/lexicons/lib/validations/index.test.ts
@@ -1,80 +1,111 @@
-import { expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import * as v from './index.js';
+import { allowsEval } from './utils.js';
 
-it('performs validation', () => {
-	const subobjectSchema = v.object({
-		$type: v.optional(v.literal('com.example.kitchenSink#subobject')),
-		boolean: v.boolean(),
-	});
+describe.each([[false], [true]])(`with eval: %p`, (withEval) => {
+	it('performs validation', () => {
+		using _mock = vi.spyOn(allowsEval, 'value', 'get').mockReturnValue(withEval);
 
-	const objectSchema = v.object({
-		$type: v.optional(v.literal('com.example.kitchenSink#object')),
-		get object() {
-			return subobjectSchema;
-		},
-		array: v.array(v.string()),
-		boolean: v.boolean(),
-		integer: v.integer(),
-		integerWithDefault: v.optional(v.integer(), 42),
-		integerWithDefaultFn: v.optional(v.integer(), () => 421),
-		string: v.string(),
-	});
+		const subobjectSchema = v.object({
+			$type: v.optional(v.literal('com.example.kitchenSink#subobject')),
+			boolean: v.boolean(),
+		});
 
-	const recordSchema = v.record(
-		v.tidString(),
-		v.object({
-			$type: v.literal('com.example.kitchenSink'),
+		const objectSchema = v.object({
+			$type: v.optional(v.literal('com.example.kitchenSink#object')),
 			get object() {
-				return objectSchema;
+				return subobjectSchema;
 			},
 			array: v.array(v.string()),
 			boolean: v.boolean(),
 			integer: v.integer(),
+			integerFilled: v.optional(v.integer()),
+			integerOptional: v.optional(v.integer()),
+			integerWithDefault: v.optional(v.integer(), 42),
+			integerWithDefaultFn: v.optional(v.integer(), () => 421),
 			string: v.string(),
+		});
 
-			atUri: v.resourceUriString(),
-			datetime: v.datetimeString(),
-			did: v.didString(),
-			cid: v.cidString(),
+		const recordSchema = v.record(
+			v.tidString(),
+			v.object({
+				$type: v.literal('com.example.kitchenSink'),
+				get object() {
+					return objectSchema;
+				},
+				array: v.array(v.string()),
+				boolean: v.boolean(),
+				integer: v.integer(),
+				string: v.string(),
 
-			bytes: v.bytes(),
-			cidLink: v.cidLink(),
-		}),
-	);
+				atUri: v.resourceUriString(),
+				datetime: v.datetimeString(),
+				did: v.didString(),
+				cid: v.cidString(),
 
-	const res: v.InferInput<typeof recordSchema> = {
-		$type: 'com.example.kitchenSink',
-		object: {
-			object: { boolean: true },
+				bytes: v.bytes(),
+				cidLink: v.cidLink(),
+			}),
+		);
+
+		const datetime = new Date().toISOString();
+
+		const res: v.InferInput<typeof recordSchema> = {
+			$type: 'com.example.kitchenSink',
+			object: {
+				object: { boolean: true },
+				array: ['one', 'two'],
+				boolean: true,
+				integer: 123,
+				integerFilled: 234,
+				string: 'string',
+			},
 			array: ['one', 'two'],
 			boolean: true,
 			integer: 123,
 			string: 'string',
-		},
-		array: ['one', 'two'],
-		boolean: true,
-		integer: 123,
-		string: 'string',
-		datetime: new Date().toISOString(),
-		atUri: 'at://did:web:example.com/com.example.test/self',
-		did: 'did:web:example.com',
-		cid: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
-		bytes: {
-			$bytes: 'AAECAw',
-		},
-		cidLink: {
-			$link: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
-		},
-	};
+			datetime: datetime,
+			atUri: 'at://did:web:example.com/com.example.test/self',
+			did: 'did:web:example.com',
+			cid: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+			bytes: {
+				$bytes: 'AAECAw',
+			},
+			cidLink: {
+				$link: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+			},
+		};
 
-	v.parse(recordSchema, res);
-});
-
-it('sets optional defaults', () => {
-	const objectSchema = v.object({
-		foo: v.optional(v.integer(), 123),
+		const result = v.parse(recordSchema, res);
+		expect(result).toEqual({
+			$type: 'com.example.kitchenSink',
+			array: ['one', 'two'],
+			atUri: 'at://did:web:example.com/com.example.test/self',
+			boolean: true,
+			bytes: {
+				$bytes: 'AAECAw',
+			},
+			cid: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+			cidLink: {
+				$link: 'bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a',
+			},
+			datetime: datetime,
+			did: 'did:web:example.com',
+			integer: 123,
+			object: {
+				array: ['one', 'two'],
+				boolean: true,
+				integer: 123,
+				integerFilled: 234,
+				integerWithDefault: 42,
+				integerWithDefaultFn: 421,
+				object: {
+					boolean: true,
+				},
+				string: 'string',
+			},
+			string: 'string',
+		});
 	});
-
-	expect(v.parse(objectSchema, {})).toEqual({ foo: 123 });
 });
