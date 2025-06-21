7b590bd70c0499a749147c27118ca3dd7bb3d00e
Mary <git@mary.my.id>
2025-06-21 10:35:50 +0700

    fix(lexicons): mark generic URIs over 8192 UTF-8 characters as invalid

---
+++ .changeset/red-monkeys-care.md
@@ -0,0 +1,5 @@
+---
+'@atcute/lexicons': patch
+---
+
+mark generic URIs over 8192 UTF-8 characters as invalid
--- packages/lexicons/lexicons/lib/syntax/uri.ts
+++ packages/lexicons/lexicons/lib/syntax/uri.ts
@@ -1,3 +1,5 @@
+import { getUtf8Length } from '../validations/utils.js';
+
 /**
  * represents a generic URI
  */
@@ -7,5 +9,31 @@ const URI_RE = /^\w+:(?:\/\/)?[^\s/][^\s]*$/;
 
 // #__NO_SIDE_EFFECTS__
 export const isGenericUri = (input: unknown): input is GenericUri => {
-	return typeof input === 'string' && input.length >= 3 && URI_RE.test(input);
+	if (typeof input !== 'string') {
+		return false;
+	}
+
+	const MIN_LENGTH = 3;
+	const MAX_LENGTH = 8192;
+
+	const utf16Len = input.length;
+	const maybeUtf8Len = utf16Len * 3;
+
+	// fail early if estimated upper bound is too small
+	if (maybeUtf8Len < MIN_LENGTH) {
+		return false;
+	}
+
+	// skip calculation if UTF-16 length already satisfies both constraints
+	if (utf16Len >= MIN_LENGTH && maybeUtf8Len <= MAX_LENGTH) {
+		return URI_RE.test(input);
+	}
+
+	const utf8Len = getUtf8Length(input);
+
+	if (utf8Len < MIN_LENGTH || utf8Len > MAX_LENGTH) {
+		return false;
+	}
+
+	return URI_RE.test(input);
 };
