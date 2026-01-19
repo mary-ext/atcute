853514cf74c4f5cb19fb824137464c987fc61b77
Mary <git@mary.my.id>
2026-01-19 11:13:00 +0700

    feat(xrpc-server): export operation types

---
+++ .changeset/stale-cats-watch.md
@@ -0,0 +1,5 @@
+---
+'@atcute/xrpc-server': patch
+---
+
+export operation types
--- packages/servers/xrpc-server/lib/main/index.ts
+++ packages/servers/xrpc-server/lib/main/index.ts
@@ -1,6 +1,17 @@
 export * from './response.js';
-export * from './xrpc-handler.js';
 export * from './router.js';
 export * from './xrpc-error.js';
+export * from './xrpc-handler.js';
 
+export type {
+	ProcedureConfig,
+	ProcedureContext,
+	ProcedureHandler,
+	QueryConfig,
+	QueryContext,
+	QueryHandler,
+	SubscriptionConfig,
+	SubscriptionContext,
+	SubscriptionHandler,
+} from './types/operation.js';
 export * from './types/websocket.js';
