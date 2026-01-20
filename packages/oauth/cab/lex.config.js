9c40d5b176e8e6cb86718f6ae4bcd9c7339bac0a
Mary <git@mary.my.id>
2026-01-20 11:00:18 +0700

    refactor(oauth-cab): publicly export getClientAssertion

--- packages/oauth/cab/lex.config.js
+++ packages/oauth/cab/lex.config.js
@@ -3,4 +3,8 @@ import { defineLexiconConfig } from '@atcute/lex-cli';
 export default defineLexiconConfig({
 	files: ['lexicons-src/**/*.ts'],
 	outdir: 'lib/lexicons/',
+	export: {
+		outdir: 'lexicons/',
+		clean: true,
+	},
 });
---
+++ packages/oauth/cab/lib/index.ts
@@ -0,0 +1,1 @@
+export * from './lexicons/index.js';
