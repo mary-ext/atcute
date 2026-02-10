16fe8a056175652e58f1c5b313b0a243f6c1b028
Mary <git@mary.my.id>
2026-02-10 14:31:53 +0700

    chore(multibase): add comment indicating polyfill removal

--- packages/utilities/multibase/lib/bases/base16-web-polyfill.ts
+++ packages/utilities/multibase/lib/bases/base16-web-polyfill.ts
@@ -1,14 +1,11 @@
+// remove after September 2027
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toHex
+
 import { createRfc4648Decode, createRfc4648Encode } from '../utils.ts';
 
 const BASE16_CHARSET = '0123456789abcdef';
 
-export const fromBase16: (str: string) => Uint8Array<ArrayBuffer> = /*#__PURE__*/ createRfc4648Decode(
-	BASE16_CHARSET,
-	4,
-	false,
-);
-export const toBase16: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
-	BASE16_CHARSET,
-	4,
-	false,
-);
+export const fromBase16: (str: string) => Uint8Array<ArrayBuffer> =
+	/*#__PURE__*/ createRfc4648Decode(BASE16_CHARSET, 4, false);
+export const toBase16: (bytes: Uint8Array) => string =
+	/*#__PURE__*/ createRfc4648Encode(BASE16_CHARSET, 4, false);
--- packages/utilities/multibase/lib/bases/base64-web-polyfill.ts
+++ packages/utilities/multibase/lib/bases/base64-web-polyfill.ts
@@ -1,56 +1,35 @@
+// remove after September 2027
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toBase64
+
 import { createRfc4648Decode, createRfc4648Encode } from '../utils.ts';
 
 const BASE64_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 const BASE64URL_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
 
 // #region base64
-export const fromBase64: (str: string) => Uint8Array<ArrayBuffer> = /*#__PURE__*/ createRfc4648Decode(
-	BASE64_CHARSET,
-	6,
-	false,
-);
-export const toBase64: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
-	BASE64_CHARSET,
-	6,
-	false,
-);
+export const fromBase64: (str: string) => Uint8Array<ArrayBuffer> =
+	/*#__PURE__*/ createRfc4648Decode(BASE64_CHARSET, 6, false);
+export const toBase64: (bytes: Uint8Array) => string =
+	/*#__PURE__*/ createRfc4648Encode(BASE64_CHARSET, 6, false);
 // #endregion
 
 // #region base64pad
-export const fromBase64Pad: (str: string) => Uint8Array<ArrayBuffer> = /*#__PURE__*/ createRfc4648Decode(
-	BASE64_CHARSET,
-	6,
-	true,
-);
-export const toBase64Pad: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
-	BASE64_CHARSET,
-	6,
-	true,
-);
+export const fromBase64Pad: (str: string) => Uint8Array<ArrayBuffer> =
+	/*#__PURE__*/ createRfc4648Decode(BASE64_CHARSET, 6, true);
+export const toBase64Pad: (bytes: Uint8Array) => string =
+	/*#__PURE__*/ createRfc4648Encode(BASE64_CHARSET, 6, true);
 // #endregion
 
 // #region base64url
-export const fromBase64Url: (str: string) => Uint8Array<ArrayBuffer> = /*#__PURE__*/ createRfc4648Decode(
-	BASE64URL_CHARSET,
-	6,
-	false,
-);
-export const toBase64Url: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
-	BASE64URL_CHARSET,
-	6,
-	false,
-);
+export const fromBase64Url: (str: string) => Uint8Array<ArrayBuffer> =
+	/*#__PURE__*/ createRfc4648Decode(BASE64URL_CHARSET, 6, false);
+export const toBase64Url: (bytes: Uint8Array) => string =
+	/*#__PURE__*/ createRfc4648Encode(BASE64URL_CHARSET, 6, false);
 // #endregion
 
 // #region base64urlpad
-export const fromBase64UrlPad: (str: string) => Uint8Array<ArrayBuffer> = /*#__PURE__*/ createRfc4648Decode(
-	BASE64URL_CHARSET,
-	6,
-	true,
-);
-export const toBase64UrlPad: (bytes: Uint8Array) => string = /*#__PURE__*/ createRfc4648Encode(
-	BASE64URL_CHARSET,
-	6,
-	true,
-);
+export const fromBase64UrlPad: (str: string) => Uint8Array<ArrayBuffer> =
+	/*#__PURE__*/ createRfc4648Decode(BASE64URL_CHARSET, 6, true);
+export const toBase64UrlPad: (bytes: Uint8Array) => string =
+	/*#__PURE__*/ createRfc4648Encode(BASE64URL_CHARSET, 6, true);
 // #endregion
