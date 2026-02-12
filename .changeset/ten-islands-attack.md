d7a8f6034dc3062258381ac656672192f764cae2
Mary <git@mary.my.id>
2026-02-12 13:34:43 +0700

    refactor(util-text): add ASCII fast-path to grapheme counting

---
+++ .changeset/ten-islands-attack.md
@@ -0,0 +1,5 @@
+---
+'@atcute/util-text': patch
+---
+
+add ASCII fast-path to grapheme counting
--- packages/misc/util-text/lib/index.rn.ts
+++ packages/misc/util-text/lib/index.rn.ts
@@ -1,11 +1,44 @@
 import { countGraphemes } from 'unicode-segmenter/grapheme';
 
+const isAsciiWithoutCr = (text: string): boolean => {
+	const len = text.length;
+	let idx = 0;
+
+	while (idx + 3 < len) {
+		const a = text.charCodeAt(idx);
+		const b = text.charCodeAt(idx + 1);
+		const c = text.charCodeAt(idx + 2);
+		const d = text.charCodeAt(idx + 3);
+
+		if ((a | b | c | d) > 0x7f || a === 0x0d || b === 0x0d || c === 0x0d || d === 0x0d) {
+			return false;
+		}
+
+		idx += 4;
+	}
+
+	while (idx < len) {
+		const code = text.charCodeAt(idx);
+		if (code > 0x7f || code === 0x0d) {
+			return false;
+		}
+
+		idx++;
+	}
+
+	return true;
+};
+
 /**
  * returns the grapheme length of a string
  * @param text string to count graphemes in
  * @returns grapheme count
  */
 export const getGraphemeLength = (text: string): number => {
+	if (isAsciiWithoutCr(text)) {
+		return text.length;
+	}
+
 	return countGraphemes(text);
 };
 
@@ -28,6 +61,10 @@ export const isGraphemeLengthInRange = (text: string, min: number, max: number):
 	// grapheme count is definitely within max
 	if (min === 0 && utf16Len <= max) {
 		return true;
+	}
+
+	if (isAsciiWithoutCr(text)) {
+		return utf16Len <= max;
 	}
 
 	const count = countGraphemes(text);
