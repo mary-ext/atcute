d7d30e43486c77435c55e9c6de4d771e80589ad9
Mary <git@mary.my.id>
2026-06-04 16:20:46 +0700

    chore(leaflet): pull latest Leaflet lexicons

--- packages/definitions/leaflet/lexicons/README.md
+++ packages/definitions/leaflet/lexicons/README.md
@@ -3,4 +3,4 @@
 this directory contains lexicon documents pulled from the following sources:
 
 - https://github.com/hyperlink-academy/leaflet.git
-  - commit: fad9ce6689322b20008cbd89bb87f8c9e5a5960e
+  - commit: c64fbf78f57d065f2c9c1e8beb9348bfe63103a5
--- packages/definitions/leaflet/lib/lexicons/types/pub/leaflet/blocks/postsList.ts
+++ packages/definitions/leaflet/lib/lexicons/types/pub/leaflet/blocks/postsList.ts
@@ -3,9 +3,9 @@ import * as v from '@atcute/lexicons/validations';
 
 const _mainSchema = /*#__PURE__*/ v.object({
 	$type: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.literal('pub.leaflet.blocks.postsList')),
-	filterByTag: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.string()),
+	filterByTags: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.array(/*#__PURE__*/ v.string())),
 	highlightFirstPost: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.boolean()),
-	view: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.string<'compact' | 'full' | (string & {})>()),
+	view: /*#__PURE__*/ v.optional(/*#__PURE__*/ v.string<'medium' | 'small' | (string & {})>()),
 });
 
 type main$schematype = typeof _mainSchema;
